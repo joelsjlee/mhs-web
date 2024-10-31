1b5a190d3bd059c97cdddb12d2e1db6bbc37397d
joelsjlee <joelsjlee@gmail.com>
2024-10-31 14:41:26 -0400

    topics.py refactoring

--- data/timeline/Updated Python/topics.py
+++ data/timeline/Updated Python/topics.py
@@ -22,7 +22,7 @@ def writeJSON(input_dict, output_filepath):
         print("Succesfully wrote to " + output_filepath)
         json.dump(input_dict, outfile, indent=4)
 
-def create_umbrellas(folder_path):
+def create_umbrella_to_topics(folder_path):
     # Get the current directory of the script
 
     """
@@ -33,82 +33,78 @@ def create_umbrellas(folder_path):
     Returns:
 
     """
-    all_topics = []
-    all_umbrellas = []
+    all_topics_from_api = []
+    all_umbrellas_from_api = []
 
-    umbrellas_dict = {}
+    umbrella_to_topic_dict = {}
 
     # Gets all topics in the API data
-    APITopics = json.loads(requests.get('https://primarysourcecoop.org/subjectsmanager/ext/getallfromdb/').text, verify=False)
+    APITopics = json.loads(requests.get('https://primarysourcecoop.org/subjectsmanager/ext/getallfromdb/', verify=False).text,)
 
 
     for topic in APITopics:
         # Adds every topic to the all_topics list
-        all_topics.append(topic["topic_name"])
+        all_topics_from_api.append(topic["topic_name"])
 
         # If umbrella, adds the topic to the umbrellas list
         if topic['is_umbrella'] == "1":
-            all_umbrellas.append(topic['topic_name'])
+            all_umbrellas_from_api.append(topic['topic_name'])
 
     # Topics that fall under a particular umbrella
-    umb_topics = []
-    for umbrella in all_umbrellas:
+    api_topics_with_umbrellas = []
+    for umbrella in all_umbrellas_from_api:
         res = requests.get('https://www.primarysourcecoop.org/subjectsmanager/getsubtopics?topic=' + umbrella, verify=False)
         data = json.loads(res.text)
 
         # list of all topics under a certain umbrella
-        topics_list = list(set().union(*(d.values() for d in data)))
+        # topics_list = list(set().union(*(d.values() for d in data)))
+        topics_list = list({value for d in data for value in d.values()})
 
         # keeps track of topics with an umbrella
-        umb_topics.extend(topics_list)
+        api_topics_with_umbrellas.extend(topics_list)
 
         # Keeps track of what topics are under what umbrella
-        umbrellas_dict[umbrella] = topics_list
+        umbrella_to_topic_dict[umbrella] = topics_list
 
     # Gets a set of all topics that do not fall under an umbrella
-    no_umb1 = set(all_topics) - set(umb_topics)
+    api_topics_no_umb = set(all_topics_from_api) - set(api_topics_with_umbrellas)
 
     # Collect all CSV files from the provided folder
     folder = Path(folder_path)
     csv_files = list(folder.glob("*.csv"))
-    all_subjects = set()
+    all_topics_from_csvs = set()
 
     for file in csv_files:
-        subjects = pd.read_csv(file)["subjects"]
-        all_subjects.update(subjects)
+        topics = pd.read_csv(file)["subjects"]
+        all_topics_from_csvs.update(topics)
 
     # All the topics in the data
-    no_umb2 = all_subjects - set(all_topics)
+    csv_topics_no_umb = all_topics_from_csvs - set(all_topics_from_api)
 
-    umbrellas_dict["Uncategorized"] = list(no_umb1 | no_umb2)
+    umbrella_to_topic_dict["Uncategorized"] = list(api_topics_no_umb | csv_topics_no_umb)
 
+    return umbrella_to_topic_dict
+
+def create_topic_to_umbrellas(umbrella_to_topic_dict):
     # Key: Topic, Value: [All the umbrellas it is under]
     topics_umbrellas = {}
-
-    # For each topic under a certain umbrella-
-    for umbrella in list(umbrellas_dict.keys()):
-
-        # If it already has an entry in the dictionary, add the new umbrella
-        for topic in umbrellas_dict[umbrella]:
-
-            if topic in topics_umbrellas:
-                topics_umbrellas[topic].append(umbrella)
-
-             # If it has not yet been added, create a new entry with the umbrella
-            else:
-                topics_umbrellas[topic] = [umbrella]
-
-
-    # Write to JSON files
-    writeJSON(umbrellas_dict, "umbrellas.json")
-    writeJSON(topics_umbrellas, "topiccategories.json")
+    for umbrella, topics in umbrella_to_topic_dict.items():
+        for topic in topics:
+            # Use setdefault to create a list if the key doesn't exist
+            topics_umbrellas.setdefault(topic, []).append(umbrella)
+    return topics_umbrellas
 
 def main():
     parser = argparse.ArgumentParser(description="Create umbrella topic mappings from API and CSV data.")
     parser.add_argument('folder', help='Folder containing CSV data files.')
-    args = parser.parse_args()
+    parser.add_argument('umbrella_topic', help='Output file for umbrella to topic file')
+    parser.add_argument('topic_umbrella', help='Output file for topic to umbrella file.')
 
-    create_umbrellas(args.folder)
+    args = parser.parse_args()
+    umbrella_to_topic_dict = create_umbrella_to_topics(args.folder)
+    topic_to_umbrellas_dict = create_topic_to_umbrellas(umbrella_to_topic_dict)
+    writeJSON(umbrella_to_topic_dict, args.umbrella_topic)
+    writeJSON(topic_to_umbrellas_dict, args.topic_umbrella)
 
 if __name__ == "__main__":
     main()
